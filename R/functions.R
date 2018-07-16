7296996cc285f416d2771a1522250b290c199912
julietakaminsky <kaminsky.julieta@gmail.com>
2018-07-16 16:36:13 -0300

    functions

---
+++ R/functions.R
@@ -0,0 +1,6 @@
+shannon <- function(species)
+{
+  prop <- species/sum(species) # calcula proporciones
+  s <- -sum(prop* log(prop))       # calcula shannon
+  return(s)                   # devuelve shannon 
+}
